082a3bb6101b5db2791367e8894761d4ac7efae4
Rashmi8920 <rashmisingh25052002@gmail.com>
2025-10-08 20:35:24 +0530

    Update DashBoard.jsx

--- src/pages/Admin/DashBoard.jsx
+++ src/pages/Admin/DashBoard.jsx
@@ -6,7 +6,7 @@ const DashBoard = () => {
   return (
     <div className="flex justify-center items-start p-10 bg-gray-50 min-h-screen">
       <div className="flex shadow-lg rounded-lg overflow-hidden bg-white min-h-[28rem] w-[82rem]">
-        <Navbar />
+       <div className="ml-6 "> <Navbar /></div>
         <Details />
       </div>
     </div>
